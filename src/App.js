f81426ea9f6be92fb0a99479b99bd99c56276eae
Deep_Menpara <54411124+Deep-Menpara@users.noreply.github.com>
2022-01-22 16:50:08 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,94 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
-
 import React,{useState} from 'react';
 import { Products } from './components/Products';
 import Item from './components/Item';
-
 function App() {
+  
   const [cartarr,setcartarr]=useState([]);
   const [ishome,setText]=useState("true");
+  const [searchitem,setSearch]=useState("");
   const [productarr,setproductarr]=useState(Products);
+  
+  const setsearchchange=(event)=>{
+    console.log("dd");
+    setSearch(event.target.value);
+  };
+  
   const loadcart=()=>{
     setText("false");
   };
+  
   const loadhome=()=>{
     setText("true");
   };
+  
+  const deletetocart=(id)=>{
+      setcartarr((oldarr)=>{
+      return oldarr.filter((item,index)=>{
+          return item.id !== id;
+      });
+  });
+  };
 
   const addtocart=(id,title,price,val,img)=>{
-    let found=0;
-    console.log(`${id}   ${title}   ${val}`);
-    setcartarr((oldarr)=>{
+    if(parseInt(val)>0)
+    {
+      let found=0;
+      setcartarr((oldarr)=>{
         return oldarr.map((item)=>{
-            if(item.cid==id)
+            if(item.id==id)
             {
                 found=1;
-                item.cquantity+=val;
-                
+                item.quantity=parseInt(item.quantity)+parseInt(val);
             }
             return item;
         });
     });
-
     if(found==0)
     {
         setcartarr((oldarr)=>{
-            const obj={cid:id,ctitle:title,cprice:price,cimg:img,cquantity:val}
+            const obj={id:id,title:title,price:price,img:img,quantity:val}
             return [...oldarr,obj]
         });
     }
-    
-
+    }
   };
 
   if(ishome=="true")
   {
     return (
       <>
-        
-
-
-        <button className='btn' onClick={loadcart}  >Cart</button>
-        
-
-
-        <div class="container">
-        
-  <div class="card-columns">
-
-
-  {
-    productarr.map((item)=>
-    {
-      return <Item key={item.id} {...item} onaddtocart={(val)=>{
-        addtocart(item.id,item.title,item.price,val,item.img);
-      }}></Item>;
-    })
-  }
-
-    
-
-
-
-
-    
-
-
-
-
-
-  </div>
-</div>
-
-
-
-
-
-
-
-
-        
+        <input className='inp' id="search" name="search" type="text" onChange={setsearchchange} placeholder="What're we looking for ?"/><input className='inp' id="search_submit" type="submit"/>
+        <div class="container cuscon" >
+          <div className='card'> <button className='btn' onClick={loadcart}  >Goto-> Cart</button>
+          </div>
+          <div class="card-columns">
+            {
+              productarr.filter((item)=>{
+              if (searchitem=="") {
+                return item;
+              }
+              else if (item.title.toLowerCase().includes(searchitem.toLowerCase())) {
+                return item;
+              }
+            }).map((item)=>{
+                return <Item key={item.id} {...item} ishome={1} onaddtocart={(val)=>{addtocart(item.id,item.title,item.price,val,item.img);}}></Item>;})
+            }
+          </div>
+        </div>
       </>
     );
   }
@@ -96,13 +84,32 @@ function App() {
   {
     return (
       <>
-        <button  className='btn'  onClick={loadhome}>Home</button>
-      
+        <div class="container cuscon">
+        <div className='card'>
+        <button  className='btn'   onClick={loadhome}>Home  </button>
+        </div>
+        <div class="card-columns">
+        {
+          cartarr.map((item)=>
+          {
+            return <Item key={item.id} ishome={0} {...item} ondeltocart={
+              ()=>{
+                deletetocart(item.id);
+              }
+            }></Item>;
+          })
+        }
+        </div>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <center><span className='spann'>Cart Total: {cartarr.reduce((total,item)=>{
+              return total+(parseInt(item.quantity)*parseInt(item.price));
+            },0)}/-</span>
+        </center>
       </>
     );
   }
-
-
 }
-
 export default App;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,23 +1,39 @@
 import React,{useState} from 'react';
 
-export default function Item({id,title,price,img, onaddtocart}) {
+export default function Item({id,title,price,img, onaddtocart,ishome,quantity,ondeltocart}) {
+  
   const [val,setval]=useState();
   
   const setvalchange=(event)=>{
     setval(event.target.value);
   };
-  return <div class="card cuscard">
-    
-  <div class="card-body text-center">
-  <img class="card-img-top setimg"  src={img} />
-    <p class="card-text">{title}</p>
-    <p class="card-text">{price}/-</p>
-    <input type='number' value={val} onChange={setvalchange}></input>
-    <button className='btn-primary add' onClick={()=>{
-        onaddtocart(val);
-        setval(0);
-    }}>Add</button>  
-  </div>
   
-</div>;
+  if (ishome==1) {
+  return <div class="card cuscard">
+          <div class="card-body text-center">
+          <img class="card-img-top setimg"  src={img} />
+            <p class="card-text">{title}</p>
+            <p class="card-text">{price}/-</p>
+            <input type='number' value={val} onChange={setvalchange}></input>
+            <button className='btn-primary add' onClick={()=>{
+                onaddtocart(val);
+                setval(0);
+            }}>Add</button>  
+          </div>
+        </div>;
+  }
+  else
+  {
+    return <div class="card cuscard">
+            <div class="card-body text-center">
+            <img class="card-img-top setimg"  src={img} />
+              <p class="card-text">{title}</p>
+              <p class="card-text">{price}/-</p>
+              <p class="card-text">{quantity}</p>
+              <button className='btn-primary add' onClick={()=>{
+                  ondeltocart();
+              }}>Delete</button>  
+            </div>
+          </div>;
+  }
 }
